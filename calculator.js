a5f782525ffe42401b0dbe08f34c3564a8095df8
Romel <200869522+rblamug@users.noreply.github.com>
2025-06-02 13:33:31 -0400

    Added function to operator buttons

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 const cal = {
-    add: (a, b) => { return (a + b) },
-    sub: (a, b) => { return (a - b) },
-    mul: (a, b) => { return (a * b) },
-    div: (a, b) => { return (a / b) }
+    add: (a, b) => { return (a + b); },
+    sub: (a, b) => { return (a - b); },
+    mul: (a, b) => { return (a * b); },
+    div: (a, b) => { return (a / b); }
 };
 
 let firstNumber = 0;
@@ -12,7 +12,11 @@ let op = "";
 function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
-            cal.add(num1, num2);
+            //displayScreen.textContent = cal.add(num1, num2);
+            console.log(cal.add(num1, num2))
+            console.log(firstNumber);
+            console.log(secondNumber);
+            console.log(op);
             break;
         case "-":
             cal.sub(num1, num2);
@@ -32,35 +36,61 @@ const numberButtons = document.querySelectorAll('.num');
 const operatorButtons = document.querySelectorAll('.operator');
 const displayScreen = document.querySelector('#display');
 let buttonsClicked = 0;
+let hasFirstNumber = false;
+let hasSecondNumber = false;
 
-// function getFirstNumber() {
-//     numberButtons.forEach(button => {
-//         button.addEventListener("click", (e) => {
-//             if (buttonsClicked < 10) {
-//                 if (displayScreen.textContent == 0) {
-//                     displayScreen.textContent = e.target.textContent;
-//                     buttonsClicked++;
-//                 } else {
-//                     displayScreen.TextContent += e.target.textContent;
-//                     buttonsClicked++;
-//                 }
-//             }
-//         })
-//     })
-// }
-
-// getFirstNumber();
-
-numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if (buttonsClicked < 10) {
-            if (displayScreen.textContent == 0) {
-                displayScreen.textContent = e.target.textContent;
-                buttonsClicked++;
-            } else {
-                displayScreen.textContent += e.target.textContent;
+function getFirstNumber() {
+    numberButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            if (buttonsClicked < 10 && hasFirstNumber == false) {
+                if (displayScreen.textContent == 0) {
+                    displayScreen.textContent = e.target.textContent;
+                } else {
+                    displayScreen.textContent += e.target.textContent;
+                }
                 buttonsClicked++;
             }
+        })
+    })
+}
+
+function getSecondNumber() {
+    numberButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            if (buttonsClicked < 10 && secondNumber == false) {
+                if (displayScreen.textContent == firstNumber) {
+                    displayScreen.textContent = e.target.textContent;
+                } else {
+                    displayScreen.textContent += e.target.textContent;
+                }
+                buttonsClicked++;
+            }
+        })
+    })
+}
+
+if(displayScreen.textContent == 0) {
+    getFirstNumber();
+}
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (hasFirstNumber == false) {
+            firstNumber = displayScreen.textContent;
+            op = e.target.textContent;
+            hasFirstNumber = true;
+            buttonsClicked = 0;
+            getSecondNumber();
+        } else if (hasFirstNumber == true && hasSecondNumber == false) {
+            hasSecondNumber = true;
+            secondNumber = displayScreen.textContent;
+            buttonsClicked = 0;
+        } else if (hasFirstNumber == true && hasSecondNumber == true) {
+            firstNumber = operate(firstNumber, secondNumber, op);
+            op = e.target.textContent;
+            hasSecondNumber = false;
+            buttonsClicked = 0;
+            getSecondNumber();
         }
     })
-})
+});
